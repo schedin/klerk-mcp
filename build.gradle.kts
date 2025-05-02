3e741d05d04178f3537548501eab49cd27f62977
Lennart Schedin <lennart.schedin@gmail.com>
2025-05-02 16:02:57 +0200

    Simpler build file, updated logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
     `maven-publish`
 }
 
-group = "com.github.klerk-framework"
+group = "dev.klerkframework"
 version = "0.1.0-SNAPSHOT"
 
 dependencies {
@@ -23,10 +21,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:${property("logback_version")}")
 
     // Testing
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit_version")}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${property("junit_version")}")
+    testImplementation(kotlin("test"))
 }
 
 tasks.test {
