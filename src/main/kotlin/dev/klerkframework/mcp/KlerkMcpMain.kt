ab53c19fe45bef19016ad263f3ecd0883e16950a
Lennart Schedin <lennart.schedin@gmail.com>
2025-05-02 16:53:58 +0200

    First try

---
+++ src/main/kotlin/dev/klerkframework/mcp/KlerkMcpMain.kt
@@ -0,0 +1,26 @@
+package dev.klerkframework.mcp
+
+import io.modelcontextprotocol.kotlin.sdk.Implementation
+import io.modelcontextprotocol.kotlin.sdk.ServerCapabilities
+import io.modelcontextprotocol.kotlin.sdk.server.Server
+import io.modelcontextprotocol.kotlin.sdk.server.ServerOptions
+import io.modelcontextprotocol.kotlin.sdk.server.mcp
+import io.modelcontextprotocol.kotlin.sdk.server.mcp
+
+fun registerMcp() {
+    mcp {
+        Server(
+            serverInfo = Implementation(
+                name = "example-sse-server",
+                version = "1.0.0"
+            ),
+            options = ServerOptions(
+                capabilities = ServerCapabilities(
+                    prompts = ServerCapabilities.Prompts(listChanged = null),
+                    resources = ServerCapabilities.Resources(subscribe = null, listChanged = null)
+                )
+            )
+        )
+    }
+}
+
