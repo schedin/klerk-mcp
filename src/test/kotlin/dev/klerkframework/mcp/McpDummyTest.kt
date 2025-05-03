9868f3385d979cd0d4df9815763db55d5c6b6074
Lennart Schedin <lennart.schedin@gmail.com>
2025-05-03 09:16:47 +0200

    Removing auto generated clases and adding dummy test

---
+++ src/test/kotlin/dev/klerkframework/mcp/McpDummyTest.kt
@@ -0,0 +1,11 @@
+package dev.klerkframework.mcp
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class DummyTest {
+    @Test
+    fun dummyTest() {
+        assertTrue(true, "This assertion should always pass")
+    }
+}
